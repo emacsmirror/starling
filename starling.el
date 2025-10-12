3a5c5daf83922cef1d6678d505cc76c7df0646ae
Joe Higton <draxil@gmail.com>
2025-10-12 22:27:52 +0100

    Very basic transaction screen
    Now you can drill down (with ret) into a transaction's details.

--- starling.el
+++ starling.el
@@ -4,7 +4,7 @@
 
 ;; Author: Joe Higton <draxil@gmail.com>
 ;; Contributors: Alex Drysdale <reissuecardboard@duck.com>
-;; Version: 0.1.1
+;; Version: 0.1.2
 ;; Homepage: https://github.com/draxil/starling-el
 ;; Keywords: banking, finance
 ;; Package-Requires: ((emacs "28") (plz "0.7.2"))"
@@ -129,8 +129,9 @@ BODY optional, body to send in the request (TODO, not actually any use for this 
           (list
            (alist-get 'uuid balance)
            (vector
-	    (alist-get 'name balance)
-            (starling--display-cash (car (alist-get 'balance balance))))))
+            (alist-get 'name balance)
+            (starling--display-cash
+             (car (alist-get 'balance balance))))))
         (starling--account-display-balances))))))
 
 (defun starling--display-cash (cash)
@@ -149,33 +150,22 @@ Also make it a string, for display purposes."
   ;; FUTURE: option to pick which balance to display
   ;; TODO: other accounts?
   ;; TODO: other name?
-  (let*
-      (
-       (main-uuid (starling--main-account-uuid))
-       (main-account (starling--do
-           'get
-           (concat
-            "api/v2/accounts/"
-            main-uuid
-            "/balance")))
-       )
-    `(
-      (
+  (let* ((main-uuid (starling--main-account-uuid))
+         (main-account
+          (starling--do
+           'get (concat "api/v2/accounts/" main-uuid "/balance"))))
+    `((
        ;; TODO: real name?
        (name . "Main account")
-       (balance . (,(alist-get
-		    'effectiveBalance
-		    main-account
-		    )))
-       (uuid .
-	     ,(starling--main-account-default-category)
-      )))))
+       (balance . (,(alist-get 'effectiveBalance main-account)))
+       (uuid . ,(starling--main-account-default-category))))))
 
 (defvar-keymap starling-spaces-mode-map
   :suppress 't
-  :parent tabulated-list-mode-map
-  "RET" #'starling--maybe-show-transactions)
-
+  :parent
+  tabulated-list-mode-map
+  "RET"
+  #'starling--maybe-show-transactions)
 
 (define-derived-mode
  starling-spaces-mode
@@ -193,41 +183,78 @@ Also make it a string, for display purposes."
   (pop-to-buffer "*Starling Spaces*" nil)
   (starling-spaces-mode)
   (setq tabulated-list-entries (starling-space-table))
-  (tabulated-list-print 1)
-  )
-
+  (tabulated-list-print 1))
 
 (defun starling--txns-since ()
-  (format-time-string "%FT00:00:00Z" (- (time-convert (current-time) 'integer) 2592000)))
+  (format-time-string "%FT00:00:00Z"
+                      (- (time-convert (current-time) 'integer)
+                         2592000)))
+
+(defvar-local starling--current-category nil
+  "the current category we're looking at in this buffer")
 
 (defun starling--maybe-show-transactions ()
   "Possibly show transactions, if we're on a line with an id."
   (interactive)
   (when (tabulated-list-get-id)
-    (starling--show-transactions
+    (let ((starling--current-category (tabulated-list-get-id)))
+      (starling--show-transactions
        (starling--do
-	'get
+        'get
 
-	;; TODO: sensible date:
-	(concat "api/v2/feed/account/" (starling--main-account-uuid) "/category/" (tabulated-list-get-id) "?changesSince=" (starling--txns-since)))
-       )))
+        ;; TODO: sensible date:
+        (concat
+         "api/v2/feed/account/"
+         (starling--main-account-uuid)
+         "/category/"
+         starling--current-category
+         "?changesSince="
+         (starling--txns-since)))
+       starling--current-category))))
+
+(defun starling--maybe-show-transaction ()
+  "Possibly show transaction details, if we're on a line with an id."
+  (interactive)
+  (when (tabulated-list-get-id)
+    (starling--show-transaction
+     (starling--do
+      'get
+
+      (concat
+       "api/v2/feed/account/"
+       (starling--main-account-uuid)
+       "/category/"
+       starling--current-category
+       "/"
+       (tabulated-list-get-id))))))
+
+(defvar-keymap starling-transactions-mode-map
+  :suppress 't
+  :parent
+  tabulated-list-mode-map
+  "RET"
+  #'starling--maybe-show-transaction)
 
 (define-derived-mode
-  starling-transactions-mode
-  tabulated-list-mode
-  "starling-transactions-mode"
-  "Mode for viewing Starling transactions."
-  ;; TODO customisable columns?
-  (setq tabulated-list-format
-	[("Who" 20 t) ("Description" 60 t) ("Category" 20 t) ("Amount" 10 t :right-align 't) ("Time" 20 t)])
-  (tabulated-list-init-header))
+ starling-transactions-mode
+ tabulated-list-mode
+ "starling-transactions-mode"
+ "Mode for viewing Starling transactions."
+ ;; TODO customisable columns?
+ (setq tabulated-list-format
+       [("Who" 20 t)
+        ("Ref" 30 t)
+        ("Category" 20 t)
+        ("Amount" 10 t :right-align 't)
+        ("Time" 20 t)])
+ (tabulated-list-init-header))
 
-
-(defun starling--show-transactions (txns)
-  "Show the current balances of your Starling Spaces for TXNS."
+(defun starling--show-transactions (txns category)
+  "Show the current balances of your Starling Spaces for TXNS in CATEGORY."
   ;; TODO space name?
   (pop-to-buffer "*Starling Trnsactions*" nil)
   (starling-transactions-mode)
+  (setq starling--current-category category)
   (setq tabulated-list-entries (starling-transactions--table txns))
   (tabulated-list-print 1))
 
@@ -242,20 +269,18 @@ Also make it a string, for display purposes."
        (starling--describe-txn txn)
        (starling--format-category (alist-get 'spendingCategory txn))
        (starling--txn-amount txn)
-       (starling--txn-time txn)
-       )))
+       (starling--txn-time txn))))
    (alist-get 'feedItems txns)))
 
 (defun starling--describe-txn (txn)
   "Describe a starling transaction TXN."
-  (concat
-   (alist-get 'reference txn)
-   ))
+  (concat (alist-get 'reference txn)))
 
 (defun starling--txn-amount (txn)
   "Present the amount of a transaction TXN."
   (concat
-   (when (equal (alist-get 'direction txn) "OUT") "-")
+   (when (equal (alist-get 'direction txn) "OUT")
+     "-")
    (starling--display-cash (alist-get 'amount txn))))
 
 (defun starling--txn-time (txn)
@@ -270,29 +295,38 @@ Also make it a string, for display purposes."
 (defun starling--get-spending-insights (&optional year month)
   "Display spending insights for a given YEAR and MONTH."
   (let ((account-uid (starling--main-account-uuid))
-        (query-year (if year year (format-time-string "%Y")))
-        (query-month (if month month (format-time-string "%B"))))
-    (starling--do 'get (concat "api/v2/accounts/"
-                               account-uid
-                               "/spending-insights/spending-category?year="
-                               query-year
-                               "&month="
-                               (upcase query-month)))))
+        (query-year
+         (if year
+             year
+           (format-time-string "%Y")))
+        (query-month
+         (if month
+             month
+           (format-time-string "%B"))))
+    (starling--do
+     'get
+     (concat
+      "api/v2/accounts/"
+      account-uid
+      "/spending-insights/spending-category?year="
+      query-year
+      "&month="
+      (upcase query-month)))))
 
 (define-derived-mode
-  starling-insights-mode
-  tabulated-list-mode
-  "starling-insights-mode"
-  "Mode for viewing Starling insights."
-  (setq tabulated-list-format
-	[("Category" 20 t)
-     ("Total Spent" 20 t :right-align 't)
-     ("Total Received" 20 t :right-align 't)
-     ("Net" 8 t :right-align 't)
-     ("" 1 t)
-     ("Percentage" 20 t :right-align 't)
-     ("# Transactions" 10 t :right-align 't)])
-  (tabulated-list-init-header))
+ starling-insights-mode
+ tabulated-list-mode
+ "starling-insights-mode"
+ "Mode for viewing Starling insights."
+ (setq tabulated-list-format
+       [("Category" 20 t)
+        ("Total Spent" 20 t :right-align 't)
+        ("Total Received" 20 t :right-align 't)
+        ("Net" 8 t :right-align 't)
+        ("" 1 t)
+        ("Percentage" 20 t :right-align 't)
+        ("# Transactions" 10 t :right-align 't)])
+ (tabulated-list-init-header))
 
 
 (defun starling-insight--total-spent (insight)
@@ -305,7 +339,9 @@ Also make it a string, for display purposes."
 
 (defun starling-insight--net-direction (insight)
   "Get the net direction of the INSIGHT."
-  (if (string-equal (alist-get 'netDirection insight) "OUT") "-" "+"))
+  (if (string-equal (alist-get 'netDirection insight) "OUT")
+      "-"
+    "+"))
 
 (defun starling-insight--net (insight)
   "Net spent for an INSIGHT."
@@ -326,14 +362,14 @@ Also make it a string, for display purposes."
      (list
       (alist-get 'spendingCategory insight)
       (vector
-       (starling--format-category (alist-get 'spendingCategory insight))
+       (starling--format-category
+        (alist-get 'spendingCategory insight))
        (starling-insight--total-spent insight)
        (starling-insight--total-recieved insight)
        (starling-insight--net insight)
        (starling-insight--net-direction insight)
        (starling-insight--percentage insight)
-       (starling-insight--transaction-count insight)
-       )))
+       (starling-insight--transaction-count insight))))
    (alist-get 'breakdown insights)))
 
 (defun starling--show-insights (insights)
@@ -348,5 +384,46 @@ Also make it a string, for display purposes."
   (interactive)
   (let ((insights (starling--get-spending-insights)))
     (starling--show-insights insights)))
+
+(define-derived-mode
+ starling-transaction-mode
+ special-mode
+ "starling-transaction-mode"
+ "Mode for viewing a starling transaction")
+
+(defun starling--show-transaction (txn)
+  "Show transaction TXN"
+  (pop-to-buffer "*Starling Transaction*" nil)
+  (starling-transaction-mode)
+  (cl-flet
+   ;; little helper to get transaction fields:
+   ((get-field (what) (alist-get what txn)))
+   (let ((inhibit-read-only 't)
+         (direction (get-field 'direction)))
+     (erase-buffer)
+     (if (equal direction "IN")
+         (starling--transaction-field
+          "From" (get-field 'counterPartyName)))
+     (if (equal direction "OUT")
+         (starling--transaction-field
+          "To" (get-field 'counterPartyName)))
+     (starling--transaction-field "Amount" (starling--txn-amount txn))
+     (starling--transaction-field "Status" (get-field 'status))
+     (starling--transaction-field "Source" (get-field 'source))
+     (starling--transaction-field "Ref" (get-field 'reference))
+     (starling--transaction-field "Time" (starling--txn-time txn))
+     (starling--transaction-field
+      "Category" (get-field 'spendingCategory))
+     ;; TODO: more fields!
+     )))
+
+(defun starling--transaction-field (label value)
+  "Show a transaction field for LABEL and VALUE, skipping nil VALUE ones."
+  (when (not (equal value 'nil))
+    (insert (propertize (format "%s: " label) 'face 'bold))
+    ;; TODO: must be a better way? But some alignment.
+    (insert (make-string (- 10 (length label)) 32))
+    (insert value)
+    (insert "\n")))
 
 (provide 'starling)
